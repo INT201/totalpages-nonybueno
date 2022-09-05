13ffcfef8fc54e3e72ee7e54c70fccbcaa59739a
nonybueno <thaninpong.non@mail.kmutt.ac.th>
2022-09-05 16:28:10 +0700

    finish

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,15 @@
 const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
+  if(arrayItems == null || undefined){
+      return undefined;
+  }else if(rowsPerPage == null || undefined){
+    return 1;
+  }else if(rowsPerPage == 0){
+    return 1;
+  }
+  else{
+    return Math.ceil(arrayItems.length/rowsPerPage)
+  }
 }
 module.exports = totalPages
